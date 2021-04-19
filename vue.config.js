1e48c444431c69ccd7b36b67b739e5cfa4cd4264
Dima <ygeneboy@gmail.com>
2021-04-19 23:30:43 +0300

    Start project

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    publicPath: './',
+  
+    pluginOptions: {
+      i18n: {
+        locale: 'y',
+        fallbackLocale: 'pl',
+        localeDir: 'locales',
+        enableInSFC: true
+      }
+    },
+  
+    css: {
+      extract: false
+    }
+  }
